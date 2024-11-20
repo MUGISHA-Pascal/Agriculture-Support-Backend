67496cdae7af579039c7bc675eda9bb3cf7bb8e2
MUGISHA-Pascal <mugishapascal2008@gmail.com>
2024-11-20 09:55:55 +0200

    backend

--- dist/routes/orderRoutes.js
+++ dist/routes/orderRoutes.js
@@ -1,11 +1,12 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = require("express");
+const orderController_1 = require("../controllers/orderController");
 const orderRoutes = (0, express_1.Router)();
-orderRoutes.get("/get_all_orders");
-orderRoutes.get("/get_order/:id");
-orderRoutes.post("/add_order");
-orderRoutes.put("/update_order/:id");
-orderRoutes.delete("/delete_order/:id");
-orderRoutes.delete("/delete_all_orders");
+orderRoutes.get("/get_all_orders", orderController_1.getAllOrder);
+orderRoutes.get("/get_order/:id", orderController_1.getOrderById);
+orderRoutes.post("/add_order", orderController_1.addOrder);
+orderRoutes.put("/update_order/:id", orderController_1.updateOrder);
+orderRoutes.delete("/delete_order/:id", orderController_1.deleteOrderById);
+orderRoutes.delete("/delete_all_orders", orderController_1.deleteAllOrder);
 exports.default = orderRoutes;
--- src/routes/orderRoutes.ts
+++ src/routes/orderRoutes.ts
@@ -1,10 +1,19 @@
 import { Router, Request, Response } from "express";
+import {
+  addOrder,
+  deleteAllOrder,
+  deleteOrderById,
+  getAllOrder,
+  getOrderById,
+  updateOrder,
+} from "../controllers/orderController";
+import { deleteCropById } from "../controllers/cropController";
 
 const orderRoutes = Router();
-orderRoutes.get("/get_all_orders");
-orderRoutes.get("/get_order/:id");
-orderRoutes.post("/add_order");
-orderRoutes.put("/update_order/:id");
-orderRoutes.delete("/delete_order/:id");
-orderRoutes.delete("/delete_all_orders");
+orderRoutes.get("/get_all_orders", getAllOrder);
+orderRoutes.get("/get_order/:id", getOrderById);
+orderRoutes.post("/add_order", addOrder);
+orderRoutes.put("/update_order/:id", updateOrder);
+orderRoutes.delete("/delete_order/:id", deleteOrderById);
+orderRoutes.delete("/delete_all_orders", deleteAllOrder);
 export default orderRoutes;
