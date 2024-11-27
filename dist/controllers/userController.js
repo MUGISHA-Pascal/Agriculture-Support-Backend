853862391938c4fef352ce441ac73e506c8e63cb
MUGISHA-Pascal <mugishapascal2008@gmail.com>
2024-11-27 10:09:27 +0200

    debugging

--- dist/controllers/userController.js
+++ dist/controllers/userController.js
@@ -3,7 +3,7 @@ Object.defineProperty(exports, "__esModule", { value: true });
 exports.fileUpload = void 0;
 const fileUpload = (req, res) => {
     if (!req.file) {
-        return res.status(400).json({ message: "No file uploaded" });
+        res.status(400).json({ message: "No file uploaded" });
     }
     res
         .status(200)
--- src/controllers/userController.ts
+++ src/controllers/userController.ts
@@ -1,7 +1,7 @@
 import { Request, Response } from "express";
 export const fileUpload = (req: Request, res: Response) => {
   if (!req.file) {
-    return res.status(400).json({ message: "No file uploaded" });
+    res.status(400).json({ message: "No file uploaded" });
   }
   res
     .status(200)
